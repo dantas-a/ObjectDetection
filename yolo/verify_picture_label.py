1dee316bd21624c3d95c35090723ed8b2a1a253c
dantas-a <alexandredant@hotmail.com>
2025-09-23 13:05:36 +0200

    v1

---
+++ yolo/verify_picture_label.py
@@ -0,0 +1,40 @@
+import os
+from PIL import Image, ImageDraw
+
+# chemins (à adapter)
+image_path = "../software/OIDv4_ToolKit/OID/Dataset/train/Vehicle registration plate/images/0a6ef05cd485f144.jpg"
+label_path = "../software/OIDv4_ToolKit/OID/Dataset/train/Vehicle registration plate/labels/0a6ef05cd485f144.txt"
+
+# ouvrir image
+img = Image.open(image_path)
+img_w, img_h = img.size
+
+# dessinateur
+draw = ImageDraw.Draw(img)
+
+# lire fichier label
+with open(label_path, "r") as f:
+    for line in f.readlines():
+        parts = line.strip().split()
+        if len(parts) != 5:
+            continue
+        class_id, x_c, y_c, w, h = map(float, parts)
+
+        # dénormalisation
+        x_c *= img_w
+        y_c *= img_h
+        w *= img_w
+        h *= img_h
+
+        # conversion centre -> coins
+        x_min = x_c - w / 2
+        y_min = y_c - h / 2
+        x_max = x_c + w / 2
+        y_max = y_c + h / 2
+
+        # dessiner rectangle
+        draw.rectangle([x_min, y_min, x_max, y_max], outline="red", width=3)
+        draw.text((x_min, y_min - 10), f"class {int(class_id)}", fill="red")
+
+# afficher
+img.show()
