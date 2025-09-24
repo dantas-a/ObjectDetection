4d619b2315774b432293b2541574dc566c2cccfb
dantas-a <alexandredant@hotmail.com>
2025-09-24 17:27:46 +0200

    Working version

--- yolo/main.py
+++ yolo/main.py
@@ -2,55 +2,96 @@ from ultralytics import YOLO
 import cv2
 
 from sort.sort import *
-from util import get_car
+from util import get_car, read_license_plate, write_csv
 
 # Load models
 vehicle_model = YOLO('yolov8n.pt')  
 license_plate_model = YOLO('saved_model/license_plate_model.pt') 
 
+results = {}
 mot_tracker = Sort()
 
 # Load a video
 cap = cv2.VideoCapture('./videos/car.mp4')
 
-vehicles = [2,3,5,7]  # vehicle class IDs in COCO dataset
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+fps = cap.get(cv2.CAP_PROP_FPS)
 
-# read frames
+fourcc = cv2.VideoWriter_fourcc(*"mp4v")  # ou "XVID"
+out = cv2.VideoWriter("result.mp4", fourcc, fps, (width, height))
+
+# vehicle class IDs 
+vehicles = [2,3,5,7] 
+
 frame_nmr = 0
 ret = True
+# go through each frame of the video
 while ret:
     frame_nmr += 1
+    # read a frame
     ret, frame = cap.read()
-    if ret and frame_nmr < 10:
-        # detect cars
+    # if the frame was properly read (not end of video)
+    if ret : 
+        # initialize results for the current frame
+        results[frame_nmr] = {}
+        
+        # detect elements in the frame (ex: vehicles, persons, etc.)
         detections = vehicle_model(frame)[0]
+        # we initialize an empty list to store only vehicle detections
         detections_ = []
+        # we go through each detection
         for detection in detections.boxes.data.tolist():
+            # box coordinates, confidence score, class id
             x1, y1, x2, y2, score, class_id = detection
+            # we keep only vehicles
             if int(class_id) in vehicles:
                 detections_.append([x1,y1,x2,y2, score])
-                
-        # track vehicles
-        track_ids = mot_tracker.update(np.array(detections_))
+           
+        # if we detected vehicles     
+        if detections_ != []:
+            # track vehicles on the frame
+            track_ids = mot_tracker.update(np.array(detections_))
         
-        # detect license plates
-        license_plates = license_plate_model(frame)[0]
-        for license_plate in license_plates.boxes.data.tolist():
-            x1, y1, x2, y2, score, class_id = license_plate
+            # detect license plates in the frame
+            license_plates = license_plate_model(frame)[0]
+            # we go through each detected license plate
+            for license_plate in license_plates.boxes.data.tolist():
+                # box coordinates, confidence score, class id of the license plate
+                x1, y1, x2, y2, score, class_id = license_plate
+                
+                # find the car associated with the license plate in the frame
+                xcar1, ycar1, xcar2, ycar2, car_id = get_car(license_plate,track_ids)
+                
+                # if we found the car
+                if car_id != -1:
+                    # get license plate image
+                    license_plate_image = frame[int(y1):int(y2), int(x1):int(x2),:]
+                    
+                    # process license plate image for the OCR
+                    # convert image to grey scale
+                    license_plate_image_grey = cv2.cvtColor(license_plate_image, cv2.COLOR_BGR2GRAY)
+                    # Everything under 120 -> 0, everything over 120 -> 255
+                    _, license_plate_image_thresh = cv2.threshold(license_plate_image_grey,120,255,cv2.THRESH_BINARY_INV)
+                    
+                    #plt.imshow(license_plate_image_thresh, cmap='gray')
+                    #plt.show()
+                    
+                    # recover the text on the license plate with an OCR 
+                    license_plate_text, confidence_text_extraction = read_license_plate(license_plate_image_thresh)
+                
+                    # we save the results for the current frame and the current car
+                    results[frame_nmr][car_id] = {
+                        'car': {'bbox' : [xcar1, ycar1, xcar2, ycar2]},
+                        
+                        'license_plate' : {'bbox': [x1, y1, x2, y2],
+                                        'bbox_score': score,
+                                        'text': license_plate_text,
+                                        'text_score': confidence_text_extraction}
+                    }
+                    
+        # Future work: display results on the frame and save the frame in a video
             
-            # assign license plates to vehicles
-            xcar1, ycar1, xcar2, ycar2, car_id = get_car(license_plate,track_ids)
-            
-            # get license plate image
-            license_plate_image = frame[int(y1):int(y2), int(x1):int(x2),:]
-            
-            # process license plate image
-            # convert image to grey scale
-            license_plate_image_grey = cv2.cvtColor(license_plate_image, cv2.COLOR_BGR2GRAY)
-            # Everything under 120 -> 0, everything over 120 -> 255
-            _, license_plate_image_thresh = cv2.threshold(license_plate_image_grey,120,255,cv2.THRESH_BINARY_INV)
-            
-            cv2.imshow("license_plate_image", license_plate_image)
-            cv2.imshow("license_plate_image_grey", license_plate_image_grey)
-            cv2.imshow("license_plate_image_thresh", license_plate_image_thresh)
-            cv2.waitKey(0)
+
+# write results
+write_csv(results, './results.csv')     
--- yolo/util.py
+++ yolo/util.py
@@ -1,7 +1,60 @@
+import string
+import easyocr
+
+# Initialize the EasyOCR reader once to avoid re-initialization overhead
+reader = easyocr.Reader(['en'], gpu=True)
+
+# Save the results in a CSV file
+def write_csv(results, output_path):
+    
+    with open(output_path, mode='w') as f:
+        f.write('{},{},{},{},{},{},{}\n'.format('frame_nmr','car_id','car_bbox','license_plate_bbox','license_plate_bbox_score','license_plate_text','license_plate_text_score'))
+        
+        for frame_nmr in results.keys():
+            for car_id in results[frame_nmr].keys():
+                if 'car' in results[frame_nmr][car_id].keys() and 'license_plate' in results[frame_nmr][car_id].keys() and 'text' in results[frame_nmr][car_id]['license_plate'].keys():
+                    f.write('{},{},{},{},{},{},{}\n'.format(frame_nmr, car_id,
+                                                            '[{} {} {} {}]'.format(results[frame_nmr][car_id]['car']['bbox'][0],results[frame_nmr][car_id]['car']['bbox'][1],results[frame_nmr][car_id]['car']['bbox'][2],results[frame_nmr][car_id]['car']['bbox'][3]),
+                                                            '[{} {} {} {}]'.format(results[frame_nmr][car_id]['license_plate']['bbox'][0],results[frame_nmr][car_id]['license_plate']['bbox'][1],results[frame_nmr][car_id]['license_plate']['bbox'][2],results[frame_nmr][car_id]['license_plate']['bbox'][3]),
+                                                            results[frame_nmr][car_id]['license_plate']['bbox_score'],
+                                                            results[frame_nmr][car_id]['license_plate']['text'],
+                                                            results[frame_nmr][car_id]['license_plate']['text_score']
+                                                            ))
+    f.close()
+
+# Get a license plate, then goes through the tracked cars to find the car associated with the license plate
 def get_car(license_plate, track_ids):
     
-    return 0,0,0,0,0
+    xlp1, ylp1, xlp2, ylp2, score, class_id = license_plate
+    found_car = False
+    
+    # we go through each tracked car
+    for j in track_ids :
+        # box coordinates and track id of the car
+        xcar1, ycar1, xcar2, ycar2, car_id = j
+        
+        # if the license plate is inside the car box, we found the car
+        if xcar1 < xlp1 and xlp2 < xcar2 and ycar1 < ylp1 and ylp2 < ycar2:
+            car = j
+            found_car = True
+            break
+    
+    if found_car:
+        return car
+    
+    return -1,-1,-1,-1,-1
 
+# Read the license plate with an OCR and return the text and the confidence of the extraction
 def read_license_plate(license_plate_image):
     
-    return None, None
+    allowlist = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 -"
+    
+    detection = reader.readtext(license_plate_image, paragraph=False, allowlist=allowlist)
+    
+    if detection != []:
+        bbox, text, confidence = detection[0]
+    else :
+        text = ''
+        confidence = 0.0
+    
+    return text, confidence
